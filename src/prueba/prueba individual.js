eb0115b061219a55e5eb289be9763603c6a772e1
Moises Ayala <mayalam1991emanuel@gmail.com>
2023-03-23 21:15:45 -0600

    23/03/23 21:14

--- src/prueba/prueba individual.js
+++ src/prueba/prueba individual.js
@@ -1,17 +1,24 @@
 //Ejercicio- obten el promedio de los estudiantes
 function promedio(listaObje) {
-    var stotal =+ number(totaldeci.toFixed(2)); //variable convertida en numero con exactitud de 2 decimales
-    var stotalprome = stotal/i;
-    for (let i = 0; i <= listaObje.length; i++) { //itero los elementos de la lista de objetos
-        var prome = listaObje[i]; //accedo a cada objeto
-        var sumanota = prome[grades]; //accedo a la propiedad
-        var total = sumaTnota / j; //calculo el promedio individual
-        var totaldeci = number(total.toFixed(2)); //coercion a number + exactitud 2 decimales 
-        for (let j = 0; j <= sumanota.length; j++) { //itero el array de la propiedad grades
-            var sumaTnota =+ sumanota[j]; //acumulo la suma de notas
+//.......para acceder a la lista de objetos se debe indexar
+    var stotal = number((stotalprome/i).toFixed(2)); //8. variable convertida en numero con exactitud de 2 decimales
+    var stotalprome =+ totaldeci; //8. acumulo la suma de notas y calculo el promedio de todos los alumnos con la variable i 
+    for (let i = 0; i <= listaObje.length; i++) { //1. itero los elementos de la lista para utilizarlos uno por uno
+        var prome = listaObje[i]; //2. accedo a cada objeto por medio del indice
+        var sumanota = prome[grades]; //3. accedo a la propiedad del objeto utilizando la notacion de corchete
+        var total = sumaTnota; //6. extraigo la suma total de la iteracion 4 y hago el calculo del promedio para un solo estudiante y utilizo la variable j
+        var totaldeci = number((total/sumanota.length).toFixed(2)); //7. ejecuto la coercion a number + exactitud 2 decimales 
+        for (let j = 0; j <= sumanota.length; j++) { //4. terminado el paso 3 accedo a la lista de la propiedad iterando los elementos
+            var sumaTnota =+ sumanota[j]; //5. itero cada elemento y lo sumo con operador de asignacion a una variable (esto para un solo estudiante).
             
         }
     }
+    for (let k = 0; k <= listaObje.length; k++) {
+        var nomb = listaObje[k];
+        var objeNom = nomb[name];
+
+    }
+//.......para crear el objeto con los elementos 
     console.log({
         classAverage: stotal, 
         students:[]})
