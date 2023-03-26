b6a648f9623fd5af85680e0d9b50f71d8fd813e0
Moises Ayala <mayalam1991emanuel@gmail.com>
2023-03-26 17:36:51 -0600

    26/03/23 05:36

--- src/prueba/pruebas-resueltas-platzi.js
+++ src/prueba/pruebas-resueltas-platzi.js
@@ -65,8 +65,7 @@ function getPetExerciseInfo(type, age) {
   }
 
 }
-//15-dibuja un tiangulo
- //forma un tiangulo isosceles
+//15-dibuja un tiangulo-si funciona
  function printTriangle(size, character) {
   let triangle = "";
   let newSize = Number(size);
@@ -92,6 +91,7 @@ function getPetExerciseInfo(type, age) {
 
   return triangle;
 }
+console.log(printTriangle(5, "8")); 
 
 //17-ENCUENTRA AL MICHI MAS FAMOSO
 function findFamousCats(cats) {
@@ -173,3 +173,32 @@ function getStudentAverage(students) {
   return rta;
 }
  
+//20 ENCUENTRA EL MAYOR PALINDROMO
+export function findLargestPalindrome(words) {
+  // Primero definimos que la palabra más larga es null
+  // Debido a que no se tiene un valor 
+  let largest = null;
+  // Después iteramos por cada una de las palabras
+  for (const word of words) {
+    // con split("") creamos un array donde cada elemento es una letra
+    // Ejemplo "hola" => ["h","o","l","a"]
+    // con reverse() "volteamos" el array
+    // Ejemplo ["h","o","l","a"] => ["a"","l","o","h"]
+    // y con .join("") volvemos a convertirlo en un string
+    // Ejemplo  ["a"","l","o","h"] => "aloh"
+    const reversedWord = word.split("").reverse().join("");
+    // La primer validación que hacemos es para ver si efectivamente es un array
+    if (reversedWord === word) {
+      // Si si lo es, primero verificamos que se reemplace la variable largest
+      // por el primer palindromo si es que no existe uno aún
+      if (largest == null || word.length > largest.length) {
+         // En caso de existir, se compara su longitud con el palindromo existente
+        largest = word;
+      }
+    }
+  }
+
+  // Al final siempre retornamos la variable largest
+  // Ya que si no existe ni uno solo en el array, su valor se mantiene como null
+  return largest;
+}
