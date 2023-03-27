8b9471f3161e9a6298d79e8147be6c480f44f266
Moises Ayala <mayalam1991@gmail.com>
2023-03-27 16:08:04 -0600

    27/03/23 16:07

--- src/prueba/prueba individual.js
+++ src/prueba/prueba individual.js
@@ -1,26 +1,19 @@
-//Ejercicio- obten el promedio de los estudiantes
-function promedio(listaObje) {
-//.......para acceder a la lista de objetos se debe indexar
-    var stotal = number((stotalprome/i).toFixed(2)); //8. variable convertida en numero con exactitud de 2 decimales
-    var stotalprome =+ totaldeci; //8. acumulo la suma de notas y calculo el promedio de todos los alumnos con la variable i 
-    for (let i = 0; i <= listaObje.length; i++) { //1. itero los elementos de la lista para utilizarlos uno por uno
-        var prome = listaObje[i]; //2. accedo a cada objeto por medio del indice
-        var sumanota = prome[grades]; //3. accedo a la propiedad del objeto utilizando la notacion de corchete
-        var total = sumaTnota; //6. extraigo la suma total de la iteracion 4 y hago el calculo del promedio para un solo estudiante y utilizo la variable j
-        var totaldeci = number((total/sumanota.length).toFixed(2)); //7. ejecuto la coercion a number + exactitud 2 decimales 
-        for (let j = 0; j <= sumanota.length; j++) { //4. terminado el paso 3 accedo a la lista de la propiedad iterando los elementos
-            var sumaTnota =+ sumanota[j]; //5. itero cada elemento y lo sumo con operador de asignacion a una variable (esto para un solo estudiante).
-            
-        }
-    }
-    for (let k = 0; k <= listaObje.length; k++) {
-        var nomb = listaObje[k];
-        var objeNom = nomb[name];
+let uno;
+var dos ;
+const tres;
 
-    }
-//.......para crear el objeto con los elementos 
-    console.log({
-        classAverage: stotal, 
-        students:[]})
+function multi () { //funcion declaracion
 
-}
+};
+
+multi();
+
+(function () {} ()) //IIFE
+
+let multi = function () { // funcion expresion
+
+};
+
+
+let multi1 = () => {}; //arrow funcion (funcion en una sola linea)
+
