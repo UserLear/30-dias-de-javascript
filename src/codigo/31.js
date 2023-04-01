c1e270acbd07f53b56d5db8109cd100943ada0c4
Moises Ayala <mayalam1991@gmail.com>
2023-04-01 12:04:14 -0600

    01/04/23 12:04 oficina

--- src/codigo/31.js
+++ src/codigo/31.js
@@ -32,3 +32,25 @@ const persona1 = {nombre1: "Juan", edad1: 30};
 const {nombre1, edad1} = persona1;
 console.log(nombre1); // Output: "Juan"
 console.log(edad1); // Output: 30
+
+//.......manejo de modulos
+import { miFuncion } from './miArchivo.js';
+export const otraFuncion = () => {}
+
+//.......nuevos metodos de arrays
+const numeros2 = [1, 2, 3, 4, 5];
+const encontrado = numeros2.find(n => n === 3);
+console.log(encontrado); // Output: 3
+
+//......manejo de promesas
+const miPromesa = new Promise((resolve, reject) => {
+  // código
+});
+miPromesa.finally(() => console.log('Promesa finalizada'));
+
+//.......try catch
+try {
+  // código que puede generar excepciones
+} catch (error) {
+  console.log(error);
+}
